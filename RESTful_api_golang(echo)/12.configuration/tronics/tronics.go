4da678084ae92dddae4b3297aa98d03caf25d60b
oppp0999 <oppp0999@naver.com>
2022-12-03 15:42:46 +0900

    20221203_1

--- RESTful_api_golang(echo)/12.configuration/tronics/tronics.go
+++ RESTful_api_golang(echo)/12.configuration/tronics/tronics.go
@@ -2,8 +2,6 @@ package tronics
 
 import (
 	"fmt"
-	"github.com/labstack/echo/v4"
-	"gopkg.in/go-playground/validator.v9"
 	"os"
 )
 
@@ -23,6 +21,6 @@ func Start() {
 	e.PUT("/products/:id", updateProduct)
 	e.POST("/products", createProduct)
 
-	e.Logger.Print(fmt.Sprintf("Listening on port %s", port))
+	e.Logger.Print(fmt.Sprintf("Listening on port %s", cfg.Port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 }
